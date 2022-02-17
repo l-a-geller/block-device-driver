9dcd657bcb0feee35d298348a64b84570afe4d23
Leonid Geller <gellerleonid7@gmail.com>
2022-02-17 19:27:49 +0300

    codestyle fixed with checkpatch & https://www.kernel.org/doc/html/v4.10/process/coding-style.html

--- device/my_device.h
+++ device/my_device.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
 #pragma once
 
 #include <linux/device.h>
@@ -13,7 +15,7 @@ struct block_dev {
 	struct request_queue *queue;
 	struct gendisk *gd;
 	struct device dev;
-    int mode; /* 0-read only, 1-read&write */
+	int mode; /* 0-read only, 1-read&write */
 };
 
 /* List of user devices & opearations with it: create, add, destroy */
@@ -22,43 +24,48 @@ struct user_device_list {
 	struct user_device_list *next;
 };
 
-struct user_device_list* node_create(struct block_dev *dev)
+struct user_device_list *node_create(struct block_dev *dev)
 {
-	struct user_device_list* lst = kmalloc(sizeof(struct user_device_list), GFP_KERNEL);
-	lst -> device = dev;
-	lst -> next = NULL;
+	struct user_device_list *lst;
+
+	lst = kmalloc(sizeof(struct user_device_list), GFP_KERNEL);
+	lst->device = dev;
+	lst->next = NULL;
 	return lst;
 }
 
-void list_add_front(struct user_device_list** old_head, struct block_dev *dev)
+void list_add_front(struct user_device_list **old_head, struct block_dev *dev)
 {
-	struct user_device_list* new_head = node_create(dev);
-	new_head -> next = *old_head;
+	struct user_device_list *new_head;
+
+	new_head = node_create(dev);
+	new_head->next = *old_head;
 	*old_head = new_head;
 }
 
-struct user_device_list* list_search_name(struct user_device_list *list, char* name)
+struct user_device_list *list_search_name(struct user_device_list *list,
+					char *name)
 {
-    const struct device *dev = &(list -> device -> dev);
-    struct user_device_list* res = NULL;
-    while (list)
-    {
-        if (!dev_name(dev) || !strncmp(dev_name(dev) , name, strlen(name)))
-        {
-            res = list;
-            break;
-        }
-        list  = list -> next;
-    }
-    return res;
+	const struct device *dev;
+	struct user_device_list *res;
+
+	dev = &(list->device->dev);
+	while (list) {
+		if (!dev_name(dev) ||
+			!strncmp(dev_name(dev), name, strlen(name))) {
+			res = list;
+			break;
+		}
+		list  = list->next;
+	}
+	return res;
 }
 
 
 void list_destroy(struct user_device_list *list)
 {
- 	if (list)
-    {
-		list_destroy(list -> next);
+	if (list) {
+		list_destroy(list->next);
 		kfree(list);
 	}
 }
@@ -68,7 +75,7 @@ static void my_dev_release(struct device *dev)
 	/* what to do on device release from a bus,
 	 * not much here because we release all devices at once
 	 * on module deinit
-    */
+	 */
 	pr_info("MYDRIVE: (device) bus device release requested\n");
 }
 
@@ -83,12 +90,15 @@ static void block_dev_release(struct gendisk *gd, fmode_t mode)
 	pr_info("MYDRIVE: (device) Device released\n");
 }
 
-int block_dev_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd, unsigned long arg)
+int block_dev_ioctl(struct block_device *bdev,
+			fmode_t mode,
+			unsigned int cmd,
+			unsigned long arg)
 {
 	return -ENOTTY;
 }
 
-static struct block_device_operations blockdev_ops = {
+static const struct block_device_operations blockdev_ops = {
 	.owner = THIS_MODULE,
 	.open = block_dev_open,
 	.release = block_dev_release,
@@ -101,43 +111,37 @@ static int do_transfer(struct request *rq, unsigned int *nr_bytes)
 	int ret = 0;
 	struct bio_vec bvec;
 	struct req_iterator iter;
-	struct block_dev *dev = rq -> q -> queuedata;
+	struct block_dev *dev = rq->q->queuedata;
 	loff_t pos = blk_rq_pos(rq) << SECTOR_SHIFT;
 	loff_t dev_size = (loff_t) (dev->capacity << SECTOR_SHIFT);
 
-	//pr_info("MYDRIVE: (request) request start from sector %lld  pos = %lld  dev_size = %lld\n", blk_rq_pos(rq), pos, dev_size);
+	/*pr_info(
+	 * "MYDRIVE: (request) request start from sector %lld pos = %lld\n",
+	 * blk_rq_pos(rq),
+	 * pos,
+	 * dev_size);
+	 */
 
 	/* Iterate through all the requests segments */
-	rq_for_each_segment(bvec, rq, iter)
-    {
+	rq_for_each_segment(bvec, rq, iter) {
 		unsigned long b_len = bvec.bv_len;
 
 		/* Get data pointer */
-		void* b_buf = page_address(bvec.bv_page) + bvec.bv_offset;
+		void *b_buf = page_address(bvec.bv_page) + bvec.bv_offset;
 
 		/* Check that we are not out of memory bounds */
 		if ((pos + b_len) > dev_size)
-        {
-		    b_len = (unsigned long) (dev_size - pos);
-		}
+			b_len = (unsigned long) (dev_size - pos);
 
-		if (rq_data_dir(rq) == WRITE )
-        {
-            if (dev -> mode)
-            {
-                pr_warning("MYDRIVE: (device) don't try to write to read only device\n");
-            }
-            else
-            {
-                /* Write data to the buffer into required position */
-                memcpy(dev -> data + pos, b_buf, b_len);
-            }
-		}
-        else
-        {
-		    /* Read data from the buffers position */
-		    memcpy(b_buf, dev -> data + pos, b_len);
-		}
+		if (rq_data_dir(rq) == WRITE) {
+			if (dev->mode)
+				pr_warn("MYDRIVE: (device) don't try to write to read only device\n");
+			else
+			/* Write data to buffer into required position */
+				memcpy(dev->data + pos, b_buf, b_len);
+		} else
+			/* Read data from the buffers position */
+			memcpy(b_buf, dev->data + pos, b_len);
 
 		pos += b_len;
 		*nr_bytes += b_len;
@@ -145,108 +149,109 @@ static int do_transfer(struct request *rq, unsigned int *nr_bytes)
 	return ret;
 }
 
-static blk_status_t queue_rq(struct blk_mq_hw_ctx *hctx, const struct blk_mq_queue_data* bd)
+static blk_status_t queue_rq(struct blk_mq_hw_ctx *hctx,
+				const struct blk_mq_queue_data *bd)
 {
 	unsigned int nr_bytes = 0;
 	blk_status_t status = BLK_STS_OK;
 
 	/* Start request serving */
-	blk_mq_start_request(bd -> rq);
+	blk_mq_start_request(bd->rq);
 
-	if (do_transfer(bd -> rq, &nr_bytes) != 0 || blk_update_request(bd -> rq, status, nr_bytes))
-    {
-        status = BLK_STS_IOERR;
-    }
+	if (do_transfer(bd->rq, &nr_bytes) != 0 ||
+			blk_update_request(bd->rq, status, nr_bytes))
+		status = BLK_STS_IOERR;
 
 	/* Stop request serving */
-	blk_mq_end_request(bd -> rq, status);
+	blk_mq_end_request(bd->rq, status);
 	return status;
 }
 
-static struct blk_mq_ops mq_ops = {
-   	.queue_rq = queue_rq,
+static const struct blk_mq_ops mq_ops = {
+	.queue_rq = queue_rq,
 };
 
-struct block_dev* allocate_device(struct block_dev **dev_pointer)
+struct block_dev *allocate_device(struct block_dev **dev_pointer)
 {
-    *dev_pointer = kmalloc(sizeof(struct block_dev), GFP_KERNEL);
-    if (!dev_pointer) return NULL;
-    return *dev_pointer;
+	*dev_pointer = kmalloc(sizeof(struct block_dev), GFP_KERNEL);
+
+	if (!dev_pointer)
+		return NULL;
+	return *dev_pointer;
 }
 
-static int create_block_device(struct block_dev **dev_pointer, char *dev_name, sector_t capacity)
+static int create_block_device(struct block_dev **dev_pointer,
+				char *dev_name,
+				sector_t capacity)
 {
-    int dev_major;
-    struct block_dev *dev = allocate_device(dev_pointer);
-    if (!dev)
-    {
-        pr_warning("MYDRIVE: (device) Unable to allocate device\n");
-        return -ENOMEM;
-    }
+	int dev_major;
+	struct block_dev *dev = allocate_device(dev_pointer);
 
-    dev_major = register_blkdev(0, dev_name);
-    if (dev_major < 0)
-    {
-        kfree(dev);
-        pr_warning("MYDRIVE: (device) Unable to get major number\n");
-        return -EBUSY;
-    }
+	if (!dev) {
+		pr_warn("MYDRIVE: (device) Unable to allocate device\n");
+		return -ENOMEM;
+	}
 
-    pr_info("MYDRIVE: (device) allocationg data\n");
-    dev -> mode = 1;
-	dev -> capacity = capacity;
-	dev -> data = kmalloc(capacity << 9, GFP_KERNEL);
-	if (!dev -> data)
-    {
-        unregister_blkdev(dev_major, dev_name);
-        kfree(dev);
-		pr_warning("MYDRIVE: (device) failed to allocate device IO buffer\n");
+	dev_major = register_blkdev(0, dev_name);
+	if (dev_major < 0) {
+		kfree(dev);
+		pr_warn("MYDRIVE: (device) Unable to get major number\n");
+		return -EBUSY;
+	}
+
+	pr_info("MYDRIVE: (device) allocating data\n");
+	dev->mode = 1;
+	dev->capacity = capacity;
+	dev->data = kmalloc(capacity << 9, GFP_KERNEL);
+	if (!dev->data) {
+		unregister_blkdev(dev_major, dev_name);
+		kfree(dev);
+		pr_warn("MYDRIVE: (device) failed to allocate device IO buffer\n");
 		return -ENOMEM;
 	}
 
 	pr_info("MYDRIVE: (device) initializing queue\n");
-	dev -> queue = blk_mq_init_sq_queue(&dev -> tag_set, &mq_ops, QUEUE_DEPTH, BLK_MQ_F_SHOULD_MERGE);
-	if (!dev -> queue)
-    {
-        unregister_blkdev(dev_major, dev_name);
-        kfree(dev);
-        kfree(dev -> data);
-		pr_warning("MYDRIVE: (device) failed to allocate device queue\n");
+	dev->queue = blk_mq_init_sq_queue(&dev->tag_set,
+					  &mq_ops,
+					  QUEUE_DEPTH,
+					  BLK_MQ_F_SHOULD_MERGE);
+	if (!dev->queue) {
+		unregister_blkdev(dev_major, dev_name);
+		kfree(dev);
+		kfree(dev->data);
+		pr_warn("MYDRIVE: (device) failed to allocate device queue\n");
 		return -ENOMEM;
 	}
-    /* Set driver's structure as user data of the queue */
-	dev -> queue-> queuedata = dev;
-	dev -> gd = alloc_disk(1);
+	/* Set driver's structure as user data of the queue */
+	dev->queue->queuedata = dev;
+	dev->gd = alloc_disk(1);
 
 	/* Set disk required flags and data */
-	dev -> gd -> flags = GENHD_FL_NO_PART_SCAN;
-	dev -> gd -> major = dev_major;
-	dev -> gd -> first_minor = 0;
-	dev -> gd -> fops = &blockdev_ops;
-	dev -> gd -> queue = dev -> queue;
-	dev -> gd -> private_data = dev;
+	dev->gd->flags = GENHD_FL_NO_PART_SCAN;
+	dev->gd->major = dev_major;
+	dev->gd->first_minor = 0;
+	dev->gd->fops = &blockdev_ops;
+	dev->gd->queue = dev->queue;
+	dev->gd->private_data = dev;
 
 	/* Set device name (is represented in /dev under this name) */
-	strcpy(dev -> gd -> disk_name, dev_name);
-	pr_info("MYDRIVE: (device) adding disk %s\n", dev -> gd -> disk_name);
+	strcpy(dev->gd->disk_name, dev_name);
+	pr_info("MYDRIVE: (device) adding disk %s\n", dev->gd->disk_name);
 
-	set_capacity(dev -> gd, capacity);
+	set_capacity(dev->gd, capacity);
 
 	/* Notify kernel about a new disk device */
-	add_disk(dev -> gd);
+	add_disk(dev->gd);
 	return 0;
 }
 
 static void delete_block_device(struct block_dev *dev)
 {
-	if (dev -> gd)
-    {
-		del_gendisk(dev -> gd);
-		put_disk(dev -> gd);
+	if (dev->gd) {
+		del_gendisk(dev->gd);
+		put_disk(dev->gd);
 	}
-	if (dev -> queue)
-    {
-        blk_cleanup_queue(dev -> queue);
-    }
-	kfree(dev -> data);
+	if (dev->queue)
+		blk_cleanup_queue(dev->queue);
+	kfree(dev->data);
 }
--- init/my_sysfs.h
+++ init/my_sysfs.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
 #pragma once
 #include "../device/my_device.h"
 
@@ -7,10 +9,11 @@
 #define DEVICE_COMMAND_CREATE "create"
 #define DEVICE_COMMAND_SETMODE "setmode"
 #define DEVICE_COMMAND_LIST "Command list:\n" \
-                            "create device: create device_name device_capacity (in sectors)\n" \
-                            "set device mode: setmode device_name device_mode (1 - read only, 0 - read & write)" \
+	"create device: create device_name device_capacity (in sectors)\n" \
+	"set device mode: setmode device_name device_mode" \
+		"(1 - read only, 0 - read & write)" \
 
-struct user_device_list *user_device_list_head = NULL;
+struct user_device_list *user_device_list_head;
 
 /* Bus configuration */
 /* match devices to drivers (name test) */
@@ -48,174 +51,207 @@ static struct my_driver mydriver = {
 int my_register_driver(struct my_driver *driver)
 {
 	int status;
-	driver -> driver.bus = &my_bus_type;
-	status = driver_register(&driver -> driver);
-	if (status) return status;
+
+	driver->driver.bus = &my_bus_type;
+	status = driver_register(&driver->driver);
+	if (status)
+		return status;
 	return 0;
 }
 
 void my_unregister_driver(struct my_driver *driver)
 {
-	driver_unregister(&driver -> driver);
+	driver_unregister(&driver->driver);
 }
 
 /* Registering & unregestering device */
-int my_register_device(struct block_dev *mydev, char* dev_name)
+int my_register_device(struct block_dev *mydev, char *dev_name)
 {
-    int res;
-	mydev -> dev.bus = &my_bus_type;
-	mydev -> dev.release = my_dev_release;
-	dev_set_name(&mydev -> dev, dev_name);
+	int res;
+
+	mydev->dev.bus = &my_bus_type;
+	mydev->dev.release = my_dev_release;
+	dev_set_name(&mydev->dev, dev_name);
 	res = device_register(&mydev->dev);
 	return res;
 }
 
 void my_unregister_device(struct block_dev *mydev)
 {
-    pr_info("MYDRIVE: (device) unregistering device...\n");
-    if (!mydev -> gd) return;
-    if (!mydev -> gd -> major || !mydev -> gd -> disk_name) return;
-    unregister_blkdev(mydev -> gd -> major, mydev -> gd -> disk_name);
-    pr_info("MYDRIVE: (device) default device unregistered\n");
-    delete_block_device(mydev);
-	device_unregister(&mydev -> dev);
-    kfree(mydev);
-    pr_info("MYDRIVE: (device) default device removed\n");
+	pr_info("MYDRIVE: (device) unregistering device...\n");
+	if (!mydev->gd)
+		return;
+	if (!mydev->gd->major || !mydev->gd->disk_name)
+		return;
+	unregister_blkdev(mydev->gd->major, mydev->gd->disk_name);
+	pr_info("MYDRIVE: (device) default device unregistered\n");
+	delete_block_device(mydev);
+	device_unregister(&mydev->dev);
+	kfree(mydev);
+	pr_info("MYDRIVE: (device) default device removed\n");
 }
 
 void my_unregister_user_devices(void)
 {
-    while (user_device_list_head)
-    {
-        my_unregister_device(user_device_list_head -> device);
-        user_device_list_head = user_device_list_head -> next;
-    }
-    list_destroy(user_device_list_head);
+	while (user_device_list_head) {
+		my_unregister_device(user_device_list_head->device);
+		user_device_list_head = user_device_list_head->next;
+	}
+	list_destroy(user_device_list_head);
 }
 
 /* Driver attributes */
 static ssize_t commands_show(struct device_driver *dev, char *buf)
 {
-    return snprintf(buf, PAGE_SIZE, "%s\n", DEVICE_COMMAND_LIST);
+	return snprintf(buf, PAGE_SIZE, "%s\n", DEVICE_COMMAND_LIST);
 }
 
-static ssize_t input_command_store(struct device_driver *dev, const char *buf, size_t count)
+static ssize_t input_command_store(struct device_driver *dev,
+				const char *buf,
+				size_t count)
 {
-	char command[count];
-	char device_name[count];
+	char *command;
+	char *device_name;
 	int device_size;
-    int status;
-    struct block_dev *device;
-	status = sscanf(buf, "%s %s %d", &command, &device_name, &device_size);
-    if (status != 3)
-    {
-        pr_warning("MYDRIVE: (commands) couldn't recognise a command\n");
-        return count;
-    }
-    pr_info("MYDRIVE: (commands) command received: %s\n", command);
+	int status;
+	struct block_dev *device;
 
-	if (!strncmp(command, DEVICE_COMMAND_CREATE, strlen(DEVICE_COMMAND_CREATE)))
-    {
-		pr_info("MYDRIVE: (commands) starting device creation (parameters: name = %s, capacity = %d)...\n", device_name, device_size);
+	command = kmalloc_array(count, sizeof(char), GFP_KERNEL);
+	device_name = kmalloc_array(count, sizeof(char), GFP_KERNEL);
 
-		if (device_size <= 0)
-        {
+	status = sscanf(buf, "%s %s %d", command, device_name, &device_size);
+	if (status != 3) {
+		pr_warn("MYDRIVE: (commands) couldn't recognise a command\n");
+		goto out_free_parameter_buffers;
+	}
+	pr_info("MYDRIVE: (commands) command received: %s\n", command);
+
+	if (!strncmp(command,
+		     DEVICE_COMMAND_CREATE,
+		     strlen(DEVICE_COMMAND_CREATE))) {
+		pr_info("MYDRIVE: (commands) starting device creation (parameters: name = %s, capacity = %d)...\n",
+			device_name,
+			device_size);
+
+		if (device_size <= 0) {
 			pr_info("MYDRIVE_COMMANDS: (userdevice) device capacity (sectors count) should be a positive integer\n");
-			return count;
-		}	
+			goto out_free_parameter_buffers;
+		}
 		/* TODO: check on device_name uniqueness
 		 * (kernel prints an error if device name repeats,
 		 * but it needs to be made to look not that scary)
-        */
+		 */
 
-        status = create_block_device(&device, device_name, device_size);
-		if (status < 0)
-        {
-            pr_warning("MYDRIVE: (userdevice) device init failed\n");
-			return count;
+		status = create_block_device(&device, device_name, device_size);
+		if (status < 0) {
+			pr_warn("MYDRIVE: (userdevice) device init failed\n");
+			goto out_free_parameter_buffers;
 		}
-        pr_info("MYDRIVE: (userdevice) device created");
-        node_create(device);
-        list_add_front(&user_device_list_head, device);
+		pr_info("MYDRIVE: (userdevice) device created");
+		node_create(device);
+		list_add_front(&user_device_list_head, device);
 		status = my_register_device(device, device_name);
-		if (status)
-        {
-			pr_warning("MYDRIVE: (userdevice) device registration on bus failed\n");
-			return count;
+		if (status) {
+			pr_warn("MYDRIVE: (userdevice) device registration on bus failed\n");
+			goto out_free_parameter_buffers;
 		}
-        pr_info("MYDRIVE: (userdevice) device registered on bus");
-	}
-    else if (!strncmp(command, DEVICE_COMMAND_SETMODE, strlen(DEVICE_COMMAND_SETMODE))) {
-        pr_info("MYDRIVE: (commands) starting device mode setting (parameters: name = %s, mode = %d)...\n", device_name, device_size);
-        if (device_size < 0 || device_size > 1)
-        {
-            pr_warning("MYDRIVE: (setmode) mode must 1 - readonly or 0 - read & write\n", device_name);
-            return count;
-        }
-        device = list_search_name(user_device_list_head, device_name) -> device;
-        if (!device)
-        {
-            pr_warning("MYDRIVE: (setmode) device with name %s not found\n", device_name);
-            return count;
-        }
-        device -> mode = device_size;
-        pr_info("MYDRIVE: (setmode) device %s mode set to %d\n", device_name, device_size);
-    }
-	else pr_info("MYDRIVE: (commands) command is not recognised. view list of available commands in commands attribute");
+		pr_info("MYDRIVE: (userdevice) device registered on bus");
+	} else if (!strncmp(command,
+			    DEVICE_COMMAND_SETMODE,
+			    strlen(DEVICE_COMMAND_SETMODE))) {
+		pr_info("MYDRIVE: (commands) starting device mode setting (parameters: name = %s, mode = %d)...\n",
+			device_name,
+			device_size);
+		if (device_size < 0 || device_size > 1) {
+			pr_warn("MYDRIVE: (setmode) mode must 1 - readonly or 0 - read & write\n");
+			goto out_free_parameter_buffers;
+		}
+		device = list_search_name(user_device_list_head,
+					  device_name)->device;
+		if (!device) {
+			pr_warn("MYDRIVE: (setmode) device with name %s not found\n",
+				device_name);
+			goto out_free_parameter_buffers;
+		}
+		device->mode = device_size;
+		pr_info("MYDRIVE: (setmode) device %s mode set to %d\n",
+			device_name,
+			device_size);
+	} else
+		pr_info("MYDRIVE: (commands) command is not recognised. view list of available commands in commands attribute");
+
+out_free_parameter_buffers:
+	kfree(command);
+	kfree(device_name);
 	return count;
 }
 DRIVER_ATTR_RO(commands);
 DRIVER_ATTR_WO(input_command);
 
-enum sysfs_registration_status  {
+enum sysfs_registration_status {
 	SYSFSREGISTRATON_OK = 0,
 	SYSFSREGISTRATON_BUS_FAILED,
 	SYSFSREGISTRATON_DRIVER_FAILED,
-	SYSFSREGISTRATON_DRIVER_ATTR_COMMANDS_FAILED,
-	SYSFSREGISTRATON_DRIVER_ATTR_INPUT_COMMAND_FAILED
+	SYSFSREGISTRATON_DRIVER_AC_FAILED,
+	SYSFSREGISTRATON_DRIVER_AI_FAILED
 };
 
-char* sysfs_registration_error_messages[SYSFS_REGISTRATION_ERROR_MESSAGES_COUNT] = {
-    [SYSFSREGISTRATON_BUS_FAILED] = "bus registration failed",
-    [SYSFSREGISTRATON_DRIVER_FAILED] = "driver registration on bus failed",
-    [SYSFSREGISTRATON_DRIVER_ATTR_COMMANDS_FAILED] = "driver commands attribute creation failed, driver initialized with no device usercreation",
-    [SYSFSREGISTRATON_DRIVER_ATTR_INPUT_COMMAND_FAILED] = "driver input_command attribute creation failed, driver initialized with no device usercreation"
+const char *sysfs_registration_error_messages
+[SYSFS_REGISTRATION_ERROR_MESSAGES_COUNT] = {
+	[SYSFSREGISTRATON_BUS_FAILED] =
+		"bus registration failed",
+	[SYSFSREGISTRATON_DRIVER_FAILED] =
+		"driver registration on bus failed",
+	[SYSFSREGISTRATON_DRIVER_AC_FAILED] =
+		"driver commands attribute creation failed, driver initialized with no device usercreation",
+	[SYSFSREGISTRATON_DRIVER_AI_FAILED] =
+		"driver input_command attribute creation failed, driver initialized with no device usercreation"
 };
 
-// Register bus, driver on a bus and driver attributes
-enum sysfs_registration_status my_sysfs_init(int is_user_device_registration_enabled) {
-	// Registering a sysfs bus
+/* Register bus, driver on a bus and driver attributes */
+enum sysfs_registration_status my_sysfs_init(
+	int is_user_device_registration_enabled)
+{
+	/* Registering a sysfs bus */
 	int status;
-	status = bus_register(&my_bus_type);
-  	if (status)
-          return SYSFSREGISTRATON_BUS_FAILED;
 
-	// Registering a driver
-	status = my_register_driver(&mydriver);
+	status = bus_register(&my_bus_type);
 	if (status)
-    {
+		return SYSFSREGISTRATON_BUS_FAILED;
+
+	/* Registering a driver */
+	status = my_register_driver(&mydriver);
+	if (status) {
 		bus_unregister(&my_bus_type);
 		return SYSFSREGISTRATON_DRIVER_FAILED;
 	}
-	
-	// Adding driver attributes (	commands - user views available commands,
-	//				input_command - user inputs commands and receives results)
-    if (is_user_device_registration_enabled)
-    {
-        status = driver_create_file(&(mydriver.driver), &driver_attr_commands);
-        if (status) return SYSFSREGISTRATON_DRIVER_ATTR_COMMANDS_FAILED;
-        status = driver_create_file(&(mydriver.driver), &driver_attr_input_command);
-        if (status) return SYSFSREGISTRATON_DRIVER_ATTR_INPUT_COMMAND_FAILED;
-    }
+
+	/* Adding driver attributes (
+	 * commands - user views available commands,
+	 * input_command - user inputs commands and receives results
+	 * )
+	 */
+	if (is_user_device_registration_enabled) {
+		status = driver_create_file(&(mydriver.driver),
+					    &driver_attr_commands);
+		if (status)
+			return SYSFSREGISTRATON_DRIVER_AC_FAILED;
+		status = driver_create_file(&(mydriver.driver),
+					    &driver_attr_input_command);
+		if (status)
+			return SYSFSREGISTRATON_DRIVER_AI_FAILED;
+	}
 	return SYSFSREGISTRATON_OK;
 }
 
-void my_sysfs_exit(int is_user_device_registration_enabled) {
-	if (is_user_device_registration_enabled)
-    {
-        driver_remove_file(&(mydriver.driver), &driver_attr_commands);
-        driver_remove_file(&(mydriver.driver), &driver_attr_input_command);
-    }
+void my_sysfs_exit(int is_user_device_registration_enabled)
+{
+	if (is_user_device_registration_enabled) {
+		driver_remove_file(&(mydriver.driver),
+				   &driver_attr_commands);
+		driver_remove_file(&(mydriver.driver),
+				   &driver_attr_input_command);
+	}
 	my_unregister_driver(&mydriver);
 	bus_unregister(&my_bus_type);
 }
